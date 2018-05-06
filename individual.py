0e0793e7545b7071a1b3602a36c5d5416442a36c
Edmund Do <edo@uvm.edu>
2018-05-06 01:15:41 -0400

    Moved position assignment to individuals

--- individual.py
+++ individual.py
@@ -8,11 +8,22 @@ from robot import Robot
 
 class Individual:
 
-    def __init__(self):
+    def __init__(self, id, positions):
+        self.id = id
+
         self.genome = numpy.random.random((10, 5)) * 2 - 1
+
+        self.position = None
+        self.gen_pos(positions)
 
     def __str__(self):
         return  "[" + str(self.id) + " " + str(self.fitness) + "]"
+
+    def gen_pos(self, positions):
+        # determine if a generated position is suitable (no collisions)
+        self.position = numpy.random.rand(1, 2)[0] * 2 - 1
+        while not self._is_pos_suitable(self.position, positions):
+            self.position = numpy.random.rand(1, 2)[0] * 2 - 1
 
     def send_to_sim(self, sim, position, b_x=0, b_y=0, b_lgt=1, b_wdt=1, rand=False):
         pcx = b_x
@@ -23,7 +34,14 @@ class Individual:
         self.robot = Robot(sim, self.genome, pcx + position[0]*b_lgt/2,
                 pcy + position[1]*b_wdt/2, 0)
 
-    def mutate(self):
+    def mutate(self, positions):
+        m_var = random.randint(0, 1)
+        if m_var:
+            self._mutate_genome()
+        else:
+            self._mutate_pos(positions)
+
+    def _mutate_genome(self):
         i = random.randint(0, len(self.genome) - 1);
         j = random.randint(0, len(self.genome[0]) - 1);
         self.genome[i][j] = random.gauss(self.genome[i][j],
@@ -33,3 +51,43 @@ class Individual:
             self.genome[i][j] = 1
         elif self.genome[i][j] < -1:
             self.genome[i][j] = -1
+
+    def _mutate_pos(self, positions):
+        factor = 1.0        # to encourage converging on a pt and settling
+        m_position = self._mutate_pos_helper(self.position)
+
+        # check for collisions, ignoring this individual's position
+        while not self._is_pos_suitable(m_position, positions, self.position):
+            factor *= 10.0
+            m_position = self._mutate_pos_helper(self.position, factor)
+
+        self.position = m_position
+
+    def _mutate_pos_helper(self, position, factor=1):
+        m_position = numpy.copy(position)
+
+        axis = random.randint(0,1)
+
+        # in case if the robot has coordingates of (0,0)
+        sd = math.fabs(position[axis])/factor if position[axis] != 0 else 0.001
+
+        m_position[axis] = random.gauss(position[axis], sd)
+        return m_position
+
+    def _is_pos_suitable(self, xypos, positions, ignore=None):
+        # when out of bounds of the plate
+        if abs(xypos[0]) > c.PLT_LENGTH/2 or abs(xypos[1] > c.PLT_WIDTH/2):
+            return False
+
+        # determine if a generated position is suitable (no collisions)
+        for indv_id in positions:
+            position = positions[indv_id]
+            if position is ignore or position is xypos:
+                continue
+            # base collision check on the robot dimensions
+            if (abs(position[0] - xypos[0]) < c.R_WIDTH + 2*c.R_WLENGTH
+                    and abs(position[1] - xypos[1]) < c.R_LENGTH + 2*c.R_RADIUS):
+                return False
+
+        return True
+
--- swarm.py
+++ swarm.py
@@ -5,7 +5,6 @@ import numpy
 
 import constants as c
 from individual import Individual
-from robot import Robot
 
 class Swarm:
 
@@ -21,21 +20,16 @@ class Swarm:
 
         # swarm
         self.swm_pop = []
-        self.positions = []
+        self.positions = {}
         self.fitness = 0
 
         Swarm.SWARM_ID += 1
 
         for i in range(c.swm_size):
-            indv = Individual()
+            indv = Individual(i, self.positions)
 
-            # determine if a generated position is suitable (no collisions)
-            p_arr = numpy.random.rand(1, 2)[0] * 2 - 1
-            while not self._is_pos_suitable(p_arr):
-                p_arr = numpy.random.rand(1, 2)[0] * 2 - 1
-
-            # append the suitable positions
-            self.positions.append(p_arr)
+            # track the positions
+            self.positions[indv.id] = indv.position
             self.swm_pop.append(indv)
 
     def __str__(self):
@@ -48,7 +42,7 @@ class Swarm:
                 play_paused = pp, dt = 0.025)
 
         for i, indv in enumerate(self.swm_pop):
-            indv.send_to_sim(self.sim, self.positions[i],
+            indv.send_to_sim(self.sim, indv.position,
                     0, 0, c.PLT_LENGTH, c.PLT_WIDTH)
         env.send_to_sim(self.sim)
 
@@ -109,51 +103,11 @@ class Swarm:
         del self.sim
 
     def mutate(self):
-        # select a random individual
-        m_indv = random.randint(0, c.swm_size - 1)
-
-        # either mutate the individual or its position
-        m_var = random.randint(0, 1)
-        if m_var:
-            self.swm_pop[m_indv].mutate()
-        else:
-            factor = 1.0        # to encourage converging on a pt and settling
-            m_position = self._mutate_pos(self.positions[m_indv])
-
-            while not self._is_pos_suitable(m_position, self.positions[m_indv]):
-                factor *= 10.0
-                m_position = self._mutate_pos(self.positions[m_indv], factor)
-
-            self.positions[m_indv] = m_position
+        for indv in self.swm_pop:
+            indv.mutate(self.positions)
+            self.positions[indv.id] = indv.position
 
         self.gen += 1
-
-    def _mutate_pos(self, position, factor=1):
-        m_position = numpy.copy(position)
-
-        axis = random.randint(0,1)
-
-        # in case if the robot has coordingates of (0,0)
-        sd = math.fabs(position[axis])/factor if position[axis] != 0 else 0.001
-
-        m_position[axis] = random.gauss(position[axis], sd)
-        return m_position
-
-    def _is_pos_suitable(self, xypos, ignore=None):
-        # when out of bounds of the plate
-        if abs(xypos[0]) > c.PLT_LENGTH/2 or abs(xypos[1] > c.PLT_WIDTH/2):
-            return False
-
-        # determine if a generated position is suitable (no collisions)
-        for position in self.positions:
-            if position is ignore or position is xypos:
-                continue
-            # base collision check on the robot dimensions
-            if (abs(position[0] - xypos[0]) < c.R_WIDTH + 2*c.R_WLENGTH
-                    and abs(position[1] - xypos[1]) < c.R_LENGTH + 2*c.R_RADIUS):
-                return False
-
-        return True
 
     def get_data(self, key=None):
         if key is not None:
